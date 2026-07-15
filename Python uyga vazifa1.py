353f1fd7a2e419b5f02919d87593ccb93527b089
Sarvarbek <matyoqubovsarvar25@gmail.com>
2026-07-15 12:03:33 +0500

    Create Python uyga vazifa1.py
    Pythonda_1-topshiriq

---
+++ Python uyga vazifa1.py
@@ -0,0 +1,28 @@
+# # salom="Hello world"
+# # print(salom)
+# xabar="Salom,nichiksiz ahvollar yaxshimi"
+# print(xabar)
+# xabar="Men bugun Urganchga keldim"
+# print(xabar)
+# class="mashina"
+# print(class)
+# radius=5
+# pi=3.14159
+# aylana_yuzi=pi*radius**2
+# print("Radiusi",radius, "ga teng aylananing yuzi=",aylana_yuzi)
+# kocha=input("ko'cha nomi")
+# mahalla=input("mahalla nomi")
+# tuman=input("tuman nomini kiririting")
+# viloyat=input("Qaysi viloyat")
+# print(kocha,mahalla,tuman,viloyat)
+kocha=input("ko'cha nomi:")
+mahalla=input("\nmahalla nomi:")
+tuman=input("\ntuman nomini kiririting:")
+viloyat=input("\nQaysi viloyat:")
+#print(f"{kocha}kocha,{mahalla}\nmahalla,{tuman}\ntuman,{viloyat}\nviloyat")
+manzil=f"{kocha}  kocha,{mahalla}  mahalla,{tuman}  tuman,{viloyat} viloyat"
+print("Manzili:",manzil)
+print(manzil.title())
+print(manzil.uper())
+print(manzil.lower())
+print(manzil.capitalize())
