bf804b36c1c722f3496bd58bfa1928a9f4fbc011
Sarvarbek <matyoqubovsarvar25@gmail.com>
2026-07-24 10:45:26 +0500

    Create Python modullari.topshiriq-10.py

---
+++ Python modullari.topshiriq-10.py
@@ -0,0 +1,40 @@
+          #1-masala
+import math
+# son=float(input("Son kiriting:"))
+# print(f"Natija:{math.ceil(son)}")
+
+          #2-masala
+# son = float(input(" Haqiqiy son kiriting: "))
+# print(f"Natija  {math.floor(son)}")
+
+#            #3-masala
+# x1 = float(input("x1 = "))
+# y1 = float(input("y1 = "))
+# x2 = float(input("x2 = "))
+# y2 = float(ut("y2 = "))inp
+# print(f"Masofa:, {math.dist((x1, y1), (x2, y2))}")
+
+            #4-masala
+# n = float(input("Musbat son kiriting: "))
+# print(f" {round(math.log10(n), 3)}")
+
+             #5-masala
+# son = float(input("Sonni  kiriting: "))
+# kub = lambda son: son**3
+# print("Kubi:", kub(son))
+
+              #6-maasala
+# son1 = float(input("1-son: "))
+# son2 = float(input("2-son: "))
+# yigindi = lambda son1, son2: son1 + son2
+# print("Yig'indi:", yigindi(son1, son2))
+
+               #7-masala
+# sonlar = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# toq = list(filter(lambda son: son % 2 != 0, sonlar))
+# print("Toq sonlar:", toq)
+
+               #8-masala
+sonlar = [1, 2, 3, 4, 5,8,5]
+kvadrat = list(map(lambda son: son**2, sonlar))
+print(" Kvadratlar:", kvadrat)
